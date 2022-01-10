f6540348946cc05ee449fce969a00eddedc1a849
dipstech1 <panja.souradeep@gmail.com>
2022-01-11 00:09:45 +0530

    login ui done

--- src/app/preauth/login/container/login-container/login-container.component.ts
+++ src/app/preauth/login/container/login-container/login-container.component.ts
@@ -7,24 +7,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login-container.component.scss']
 })
 export class LoginContainerComponent implements OnInit {
-
-  loginForm!: FormGroup;
-  constructor(private fb: FormBuilder) { }
-
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    })
+    
   }
 
-  get getControl() {
-    return this.loginForm.controls;
-  }
-
-  onSubmit() {
-    console.log(this.loginForm.value);
-
-  }
+ 
 
 }
--- src/app/preauth/login/login.module.ts
+++ src/app/preauth/login/login.module.ts
@@ -5,12 +5,14 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginContainerComponent } from './container/login-container/login-container.component';
 import { LoginComponent } from './component/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SocialLoginComponent } from './component/social-login/social-login.component';
 
 
 @NgModule({
   declarations: [
     LoginContainerComponent,
-    LoginComponent
+    LoginComponent,
+    SocialLoginComponent
   ],
   imports: [
     CommonModule,
