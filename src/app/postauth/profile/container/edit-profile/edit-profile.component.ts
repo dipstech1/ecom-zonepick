8c118e453bd89bf44dbb73e82f1ec80ab54558d6
dipstech1 <panja.souradeep@gmail.com>
2022-02-12 09:44:04 +0530

    edit profile ui

--- src/app/postauth/profile/container/edit-profile/edit-profile.component.ts
+++ src/app/postauth/profile/container/edit-profile/edit-profile.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
-
+  url:any = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelectFile(event:any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event?.target?.result;
+        console.log(this.url);
+        
+      }
+    }
+  }
+  public delete(){
+    this.url = '';
   }
 
 }
