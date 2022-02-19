7fea68282f075582b9fab304dacc222b983df6ac
dipstech1 <panja.souradeep@gmail.com>
2022-02-19 23:37:47 +0530

    unsubscribe added

--- src/app/postauth/products/container/product-details-container/product-details-container.component.ts
+++ src/app/postauth/products/container/product-details-container/product-details-container.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/core/service/authentication.service';
 import { ToasterService } from 'src/app/core/service/toaster.service';
 import { ProductFacade } from '../../facade/product.facade';
@@ -10,18 +11,22 @@ import { ProductFacade } from '../../facade/product.facade';
   templateUrl: './product-details-container.component.html',
   styleUrls: ['./product-details-container.component.scss']
 })
-export class ProductDetailsContainerComponent implements OnInit {
+export class ProductDetailsContainerComponent implements OnInit, OnDestroy {
 
   getRouterParam$!: Observable<Params>;
   productDetails!: any;
   productId: string = '';
   userId:string = '';
+  private ngUnsubscribe = new Subject();
+
   constructor(private activatedRoutes: ActivatedRoute, private productFacade: ProductFacade, 
     private authService:AuthenticationService, private toaster:ToasterService) { }
 
   ngOnInit(): void {
     this.getRouterParam$ = this.activatedRoutes.params;
-    this.getRouterParam$.subscribe(param => {
+    this.getRouterParam$
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe(param => {
       this.productId = param.id;
       this.getProductDetails(this.productId);
     });
@@ -29,7 +34,9 @@ export class ProductDetailsContainerComponent implements OnInit {
     this.userId = this.authService.getUserId()
   }
   getProductDetails(id: string) {
-    this.productFacade.getProductById(id).subscribe((res: any) => {
+    this.productFacade.getProductById(id)
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe((res: any) => {
       this.productDetails = res[0];
       console.log(this.productDetails);
 
@@ -44,12 +51,19 @@ export class ProductDetailsContainerComponent implements OnInit {
       _id,
       "quantity": 1,
       purpose
-    }).subscribe((res:any) => {
+    })
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe((res:any) => {
      if(res.acknowledge){
         this.toaster.showSuccess(`${name} added to cart`, "Item added successfully")
      }
       
     })
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
 }
